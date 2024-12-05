378ca6907086288d1d6e1d0a88c8aabe975a2bcc
Nizar-1012373 <1012373@hr.nl>
2024-12-05 21:21:50 +0100

    empty propellor class. should first write a test though

--- Program.cs
+++ Program.cs
@@ -10,13 +10,7 @@ namespace Drone
     {
         static void Main(string[] args)
         {   
-            Pirate pirate1 = new Pirate("Donquixote Doflamingo", "Donquixote pirates",110);
-            Pirate pirate2 = new Pirate("Red hair Shanks", "Red hair pirates",200);
-            Pirate pirate3 = new Pirate("Franky", "Straw hat pirates",50);
-
-            Console.WriteLine("the pirate "+pirate1.name+" of the "+pirate1.group+" has Conqures haki "+pirate1.HasConquerorshaki());
-            Console.WriteLine("the pirate "+pirate2.name+" of the "+pirate2.group+" has Conqures haki "+pirate2.HasConquerorshaki());
-            Console.WriteLine("the pirate "+pirate3.name+" of the "+pirate3.group+" has no Conqures haki "+pirate3.HasConquerorshaki());
+            
 
         }
         
---
+++ propellor.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//constructors
+namespace Drone
+{
+
+  class Propellor
+  {
+    public string? wing1;
+
+    public string? wing2;
+
+    public string? wing3;
+
+    public string? wing4;
+  }
+
+  public void propellor(string aWing1,string aWing2,string aWing3,string aWing4){
+        wing1 = aWing1;
+        wing2 = aWing2;
+        wing3 = aWing3;
+        wing4 = aWing4;
+      }
+
+ 
+}
