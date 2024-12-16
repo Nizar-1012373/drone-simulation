679099b6b0309da51036cd4bc500eafff3c1f4b7
Nizar-1012373 <1012373@hr.nl>
2024-12-16 18:25:36 +0100

    second test fails because isSpeedingUp method does not exists

--- PrimeService.Tests/UnitTest1.cs
+++ PrimeService.Tests/UnitTest1.cs
@@ -6,12 +6,21 @@ namespace Prime.UnitTests.Services
     public class TestMotor
     {
         [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
+        public void TestIfMotorStarts()
         {
             var motor = new Motor();
-            bool result = motor.IsStarting(false);
+            bool result = motor.IsStarting(true);
+            
 
             Assert.True(result, "The motor is not starting");
+        }
+
+        public void TestIfMotorIsSpeedingUp(){
+            var motor = new Motor();
+            int expectedResult = motor.IsSpeedingUp(100);
+            int actualResult = 20;
+
+            Assert.Equal(expectedResult,actualResult);
         }
     }
 }
